eaff7a17940005f9d308fb0fad7f0de748aff2bd
Frank Hu <jinfanfrank@gmail.com>
2025-06-01 23:55:40 -0400

    hehe so I DIDN'T need to rename all those files...

--- remove_aligned.py
+++ remove_aligned.py
@@ -2,15 +2,15 @@ import os
 import re
 
 # Set this to the path where your files are 
-directory = r"C:\Users\jinfa\Desktop\Dr. Newman CUNY\NonBuggedAudio"
+directory = r"C:\Users\jinfa\Desktop\CONYCE\Force Aligned Textgrids"
 
-pattern = re.compile(r'^conyce_aligned_(.+)\.wav$')
+pattern = re.compile(r'^conyce_aligned_(.+)\.TextGrid$')
 
 for filename in os.listdir(directory):
     match = pattern.match(filename)
     if match:
         id_part = match.group(1)
-        new_filename = f'conyce_{id_part}.wav'
+        new_filename = f'conyce_{id_part}.TextGrid'
         src = os.path.join(directory, filename)
         dst = os.path.join(directory, new_filename)
         os.rename(src, dst)
---
+++ replace_aligned.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+# Set this to the path where your files are located
+directory = '/path/to/your/files'
+
+# Regular expression to match the simplified pattern
+pattern = re.compile(r'^conyce_(?!aligned_)(.+)\.wav$')
+
+for filename in os.listdir(directory):
+    match = pattern.match(filename)
+    if match:
+        id_part = match.group(1)
+        new_filename = f'conyce_aligned_{id_part}.wav'
+        src = os.path.join(directory, filename)
+        dst = os.path.join(directory, new_filename)
+        os.rename(src, dst)
+        print(f'Renamed: {filename} -> {new_filename}')
